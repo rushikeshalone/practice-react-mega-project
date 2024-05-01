42f1b28f6e5de8f409349ea39716660087364d94
rushikeshalone <rushikeshalone.me@sbjit.edu.in>
2024-05-02 01:08:47 +0530

    Thursday

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import container from "./Container/Container";
+import Logo from "../Logo";
 
-export{Header, Footer }
+export{Header, Footer,container,Logo }
 
